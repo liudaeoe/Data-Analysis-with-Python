e5bfe74c3efef3fb41b6713510cd5a46ceff2f52
liudaeoe <2556569178@qq.com>
2024-10-20 23:27:09 +0300

    Update submission_template01.py

--- Deep Learning/submission_template01.py
+++ Deep Learning/submission_template01.py
@@ -3,13 +3,23 @@ import torch
 from torch import nn
 
 def create_model():
-    # your code here
+    # model = nn.Sequential(
+        nn.Linear(784, 256), 
+        nn.ReLU(),
+        nn.Linear(256, 16),
+        nn.ReLU(), 
+        nn.Linear(16, 10) 
+    ) 
     # return model instance (None is just a placeholder)
 
     return None
 
 def count_parameters(model):
+    #def count_parameters(model):
     # your code here
+
+    # верните количество параметров модели model
+   return sum(param.numel() for param in model.parameters())
     # return integer number (None is just a placeholder)
     
     return None
